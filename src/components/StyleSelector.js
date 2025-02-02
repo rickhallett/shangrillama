18a070269f2e0ca59d59dbec0ffda617d7c532cb
Rick Hallett <rickhallett@icloud.com>
2025-02-02 10:54:37 +0000

    refactor: Update name in StyleSelector component

--- src/components/StyleSelector.js
+++ src/components/StyleSelector.js
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types';
 
 export function StyleSelector({ onStyleSelect }) {
   const styles = ['formal', 'funny', 'flirty', 'outrageous'];
-  
+
   return (
     <>
-      <h1>You have but one chance, Ellie</h1>
+      <h1>You have but one chance, Monta.</h1>
       <h2>Choose your preferred style:</h2>
       <div className="style-buttons">
         {styles.map(style => (
